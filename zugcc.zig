4bd5821abea95351e7690168eb3ca8c4c7df588f
yoshitsugu <yoshitsugu@users.noreply.github.com>
2020-10-31 05:06:47 +0900

    +と-の演算

--- zugcc.zig
+++ zugcc.zig
@@ -3,12 +3,56 @@ const std = @import("std");
 const stdout = std.io.getStdOut().outStream();
 const print = stdout.print;
 
+fn is_number(c: u8) bool {
+    return '0' <= c and c <= '9';
+}
+
+fn expect_number(ptr: [*:0]const u8, index: usize) usize {
+    if (is_number(ptr[index])) {
+        return parse_number(ptr, index);
+    } else {
+        const errorMessage = std.fmt.bufPrint("", "数値ではありません: {}", .{ptr[index]}) catch "数値ではありません";
+        @panic(errorMessage);
+    }
+}
+
+fn parse_number(ptr: [*:0]const u8, index: usize) usize {
+    var end = index;
+    while (is_number(ptr[end])) {
+        end += 1;
+    }
+    return end;
+}
+
 pub fn main() !void {
     if (std.os.argv.len != 2) {
-        @panic("Argument length must be 2");
+        @panic("コンパイルしたい文字列を1つ引数として渡してください");
     }
+
     try print("  .globl main\n", .{});
     try print("main:\n", .{});
-    try print("  mov ${}, %rax\n", .{std.os.argv[1]});
+
+    const arg = std.os.argv[1];
+    var i: usize = 0;
+    {
+        const j = i;
+        i = expect_number(arg, i);
+        try print("  mov ${}, %rax\n", .{arg[j..i]});
+    }
+    while (arg[i] != 0) {
+        if (arg[i] == '+') {
+            i += 1;
+            const j = i;
+            i = expect_number(arg, i);
+            try print("  add ${}, %rax\n", .{arg[j..i]});
+        } else if (arg[i] == '-') {
+            i += 1;
+            const j = i;
+            i = expect_number(arg, i);
+            try print("  sub ${}, %rax\n", .{arg[j..i]});
+        } else {
+            @panic(try std.fmt.bufPrint("", "不正な文字です: {}", .{arg[i]}));
+        }
+    }
     try print("  ret\n", .{});
 }
